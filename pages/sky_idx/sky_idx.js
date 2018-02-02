2b89b58595e61e338d9cc692b1f441494073b057
dan <yaoxing.bjtu.edu.cn>
2018-02-02 17:09:07 +0800

    第一个小程序，测试了一些基本功能，获取手机型号，用户信息，点击按钮函数等

---
+++ pages/sky_idx/sky_idx.js
@@ -0,0 +1,136 @@
+// pages/sky_idx/sky_idx.js
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    css1_var: "my_css_1",
+    userInfoVar: {},
+    myPhone: {'k1': 'v1'},
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('哈哈哈，load成功，准备加载头像咯...');
+    // myapp.getUserInfo(function(userInfo){
+    //   this.setData({
+    //     userInfoVar: userInfo
+    //   })
+    // })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfoVar: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfoVar: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfoVar: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  skyClickText1: function () {
+    var now_date = new Date();
+    console.log('你点击了文字' + now_date.toLocaleString());
+    if (this.data.css1_var=='my_css_1') {
+      this.data.css1_var = 'my_css_2';
+    } else {
+      this.data.css1_var = 'my_css_1';
+    }
+    color_flag=!color_flag;
+    console.log('color='+this.data.css1_var);
+    this.setData({
+      css1_var: this.data.css1_var
+    });
+  },
+
+  get_system_info_func: function() {
+    var that=this
+    var myPhoneVar = this.data.myPhone
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log(res.model)
+        console.log(res.platform)
+        console.log(myPhoneVar)
+        // 这里是this和上一级的this不同，所以先赋值
+        myPhoneVar.model = res.model
+        that.setData({
+          myPhone: myPhoneVar
+        })
+      },
+    })
+
+  },
+
+})
+
+var color_flag=true;
+
